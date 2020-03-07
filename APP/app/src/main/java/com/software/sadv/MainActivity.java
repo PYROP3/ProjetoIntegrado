1495c56d0830158cbbed3575bcabbc44ebe15ffd
BrunoMarini <bgmarini@hotmail.com>
2020-03-07 12:15:10 -0300

    Hello World

---
+++ APP/app/src/main/java/com/software/sadv/MainActivity.java
@@ -0,0 +1,15 @@
+package com.software.sadv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        int a;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
