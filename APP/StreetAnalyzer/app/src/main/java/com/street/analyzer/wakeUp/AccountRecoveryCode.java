86b1b6c17b31f96d46708e3bfee936a8b9f62aff
Jeff <jeffersonmeneses98@gmail.com>
2020-06-11 19:45:25 -0300

    Adding Forgot your password Interface (Still in progress)

---
+++ APP/StreetAnalyzer/app/src/main/java/com/street/analyzer/wakeUp/AccountRecoveryCode.java
@@ -0,0 +1,16 @@
+package com.street.analyzer.wakeUp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.street.analyzer.R;
+
+public class AccountRecoveryCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_recovery_code);
+    }
+}
