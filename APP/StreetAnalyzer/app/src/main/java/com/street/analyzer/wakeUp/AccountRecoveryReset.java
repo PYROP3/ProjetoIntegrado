08ae95e9189b66c73ae8147d9e0456330e4479e8
Jeff <jeffersonmeneses98@gmail.com>
2020-06-12 21:31:10 -0300

    Adding 2nd Forgot your password screen

---
+++ APP/StreetAnalyzer/app/src/main/java/com/street/analyzer/wakeUp/AccountRecoveryReset.java
@@ -0,0 +1,16 @@
+package com.street.analyzer.wakeUp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.street.analyzer.R;
+
+public class AccountRecoveryReset extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_recovery_reset);
+    }
+}
